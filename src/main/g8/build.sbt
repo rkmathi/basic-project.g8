e6024f3c95237d6049ac8203bd375665756132a0
Hirokazu NISHIOKA <hiro@nisshiee.org>
2012-05-20 19:06:57 +0900

    add scala-time into build.sbt

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -10,6 +10,7 @@ libraryDependencies := Seq(
    "org.scalaz" %% "scalaz-core" % "6.0.4"
   ,"com.github.scala-incubator.io" %% "scala-io-core" % "0.3.0"
   ,"com.github.scala-incubator.io" %% "scala-io-file" % "0.3.0"
+  ,"org.scala-tools.time" %% "time" % "0.5"
   ,"org.specs2" %% "specs2" % "1.9" % "test"
   ,"org.mockito" % "mockito-all" % "1.9.0" % "test"
   ,"junit" % "junit" % "4.10" % "test"
@@ -24,4 +25,5 @@ import Scalaz._
 import scalax.io._
 import scalax.file._
 import Path._
+import org.scala_tools.time.Imports._
 """
--- src/main/g8/src/test/scala/AppSpec.scala
+++ src/main/g8/src/test/scala/AppSpec.scala
@@ -2,12 +2,12 @@ import org.specs2._
 
 class AppSpec extends Specification { def is =
 
-    "文字列'Hello world'に関するテスト"                                         ^
+    "test about String 'Hello world'"                                           ^
                                                                                 p^
-    "文字列'Hello world'は"                                                     ^
-      "長さが11であること"                                                      ! e1^
-      "'Hello'で始まること"                                                     ! e2^
-      "'world'で終わること"                                                     ! e3^
+    "'Hello world'"                                                             ^
+      "has 11 length"                                                           ! e1^
+      "starts with 'Hello'"                                                     ! e2^
+      "ends with 'world'"                                                       ! e3^
                                                                                 end
 
     def e1 = "Hello world" must have size(11)
