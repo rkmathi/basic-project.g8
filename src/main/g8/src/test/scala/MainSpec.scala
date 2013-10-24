e47783b1ccb1b425dd9dec4c7dbc6646c57523a1
Ryuichi KAWAMATA (@rkmathi) <iam@rkmathi.net>
2013-10-24 12:12:30 +0900

    Fix => MainSpec.scala

--- src/main/g8/src/test/scala/MainSpec.scala
+++ src/main/g8/src/test/scala/MainSpec.scala
@@ -6,9 +6,9 @@ class AppSpec extends Specification {
     This is a specification to check the 'Hello world' string
 
     The 'Hello world' string should
-      contain 11 characters                                                 $e1
-      start with 'Hello'                                                    $e2
-      end with 'world'                                                      $e3
+      contain 11 characters                                             $e1 \
+      start with 'Hello'                                                $e2 \
+      end with 'world'                                                  $e3 \
   """
 
   def e1 = "Hello world" must have size(11)
